d9e07c4b28e0a97942f3e51bc0e48468ed9f139f
P3trur0 <ubaldopescatore@gmail.com>
2021-10-27 14:55:20 +0200

    generated code added to repo

---
+++ generated/src/main/scala/it/pagopa/pdnd/interop/uservice/attributeregistrymanagement/model/AttributesResponse.scala
@@ -0,0 +1,12 @@
+package it.pagopa.pdnd.interop.uservice.attributeregistrymanagement.model
+
+
+/**
+ * = AttributesResponse =
+ *
+ * @param attributes  for example: ''null''
+*/
+final case class AttributesResponse (
+  attributes: Seq[Attribute]
+)
+
