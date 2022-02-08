0f7f1e40a3b5f08f63eaf43660bfd794cf6315e6
Stefano Perazzolo <stefano.perazzolo@pagopa.it>
2022-02-08 15:14:40 +0100

    change config

--- src/main/scala/it/pagopa/pdnd/interop/uservice/attributeregistrymanagement/common/system/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/attributeregistrymanagement/common/system/ApplicationConfiguration.scala
@@ -8,10 +8,10 @@ object ApplicationConfiguration {
 
   lazy val config: Config = ConfigFactory.load()
 
-  lazy val serverPort: Int = config.getInt("uservice-attribute-registry-management.port")
+  lazy val serverPort: Int = config.getInt("attribute-registry-management.port")
 
   lazy val jwtAudience: Set[String] =
-    config.getStringList("uservice-attribute-registry-management.jwt.audience").asScala.toSet
+    config.getStringList("attribute-registry-management.jwt.audience").asScala.toSet
 
   lazy val partyProxyUrl: String = config.getString("services.party-proxy")
 
