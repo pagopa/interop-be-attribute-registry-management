295744d56e1b57c659b34d654b3e2a2c4fd6836e
P3trur0 <ubaldopescatore@gmail.com>
2022-03-07 17:42:32 +0100

    enabling projections

--- src/main/scala/it/pagopa/interop/attributeregistrymanagement/common/system/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/interop/attributeregistrymanagement/common/system/ApplicationConfiguration.scala
@@ -8,7 +8,10 @@ object ApplicationConfiguration {
 
   lazy val config: Config = ConfigFactory.load()
 
-  lazy val serverPort: Int = config.getInt("attribute-registry-management.port")
+  lazy val serverPort: Int             = config.getInt("attribute-registry-management.port")
+  lazy val numberOfProjectionTags: Int = config.getInt("attribute-registry-management.projection-tags")
+
+  def projectionTag(index: Int) = s"interop-be-attribute-management-persistence|$index"
 
   lazy val jwtAudience: Set[String] =
     config.getStringList("attribute-registry-management.jwt.audience").asScala.toSet
