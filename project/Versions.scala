a6da88c078f27e083c4a9a038c84b002a0baa03a
Scala Steward <scala-steward@pagopa.it>
2022-03-23 17:12:24 +0000

    Update akka-management, ... to 1.1.3 in 0.1.x

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   lazy val akkaHttpVersion        = "10.2.6"
-  lazy val akkaManagementVersion  = "1.0.10"
+  lazy val akkaManagementVersion  = "1.1.3"
   lazy val akkaVersion            = "2.6.17"
   lazy val catsVersion            = "2.7.0"
   lazy val httpJson4sVersion      = "1.38.2"
