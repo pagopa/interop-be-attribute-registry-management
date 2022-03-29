f01cea2a7aaaeced1ff0d7da535ae6e421df8b72
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2022-03-29 17:27:18 +0200

    pin-1175: Pass bearer token to party registry (#41)

--- src/main/scala/it/pagopa/interop/attributeregistrymanagement/service/impl/PartyRegistryServiceImpl.scala
+++ src/main/scala/it/pagopa/interop/attributeregistrymanagement/service/impl/PartyRegistryServiceImpl.scala
@@ -2,7 +2,7 @@ package it.pagopa.interop.attributeregistrymanagement.service.impl
 
 import it.pagopa.interop.attributeregistrymanagement.service.{PartyProxyInvoker, PartyRegistryService}
 import it.pagopa.interop.partyregistryproxy.client.api.CategoryApi
-import it.pagopa.interop.partyregistryproxy.client.invoker.ApiRequest
+import it.pagopa.interop.partyregistryproxy.client.invoker.{ApiRequest, BearerToken}
 import it.pagopa.interop.partyregistryproxy.client.model.Categories
 import org.slf4j.{Logger, LoggerFactory}
 
@@ -13,7 +13,7 @@ final case class PartyRegistryServiceImpl(invoker: PartyProxyInvoker, api: Categ
   implicit val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
   override def getCategories(bearerToken: String): Future[Categories] = {
-    val request: ApiRequest[Categories] = api.getCategories(origin = None)
+    val request: ApiRequest[Categories] = api.getCategories(origin = None)(BearerToken(bearerToken))
     logger.info(s"getCategories ${request.toString}")
     invoker.invoke(request, "Retrieving categories")
   }
