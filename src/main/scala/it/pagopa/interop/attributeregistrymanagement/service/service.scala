a462971a7ff11e2473c2b37a7de4bfc4a57f7756
Antonio Gelameris <toniogela89@gmail.com>
2022-07-11 18:16:53 +0200

    Adding perf parameters (#99)
    * Adding perf parameters
    
    * Adding perf parameters

--- src/main/scala/it/pagopa/interop/attributeregistrymanagement/service/service.scala
+++ src/main/scala/it/pagopa/interop/attributeregistrymanagement/service/service.scala
@@ -2,11 +2,13 @@ package it.pagopa.interop.attributeregistrymanagement
 
 import akka.actor.ActorSystem
 import it.pagopa.interop.partyregistryproxy.client.invoker.ApiInvoker
+import scala.concurrent.ExecutionContextExecutor
 
 package object service {
   type PartyProxyInvoker = ApiInvoker
   object PartyProxyInvoker {
-    def apply()(implicit actorSystem: ActorSystem): PartyProxyInvoker = ApiInvoker()
+    def apply(blockingEc: ExecutionContextExecutor)(implicit actorSystem: ActorSystem): PartyProxyInvoker =
+      ApiInvoker(blockingEc)(actorSystem)
   }
 
 }
