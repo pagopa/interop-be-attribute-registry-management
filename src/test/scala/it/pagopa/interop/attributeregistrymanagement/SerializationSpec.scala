1631151534c5bb8ec9f4148399e1f88632d1e54e
Antonio Gelameris <toniogela89@gmail.com>
2022-07-20 14:14:58 +0200

    Added serde test for state

--- src/test/scala/it/pagopa/interop/attributeregistrymanagement/SerializationSpec.scala
+++ src/test/scala/it/pagopa/interop/attributeregistrymanagement/SerializationSpec.scala
@@ -3,21 +3,36 @@ package it.pagopa.interop.attributeregistrymanagement
 import cats.implicits._
 import org.scalacheck.Prop.forAll
 import org.scalacheck.Gen
-import cats.kernel.Eq
+// import cats.kernel.Eq
 import munit.ScalaCheckSuite
 import it.pagopa.interop.attributeregistrymanagement.model.persistence.attribute._
 import it.pagopa.interop.attributeregistrymanagement.model.persistence.serializer.v1.attribute.AttributeKindV1
 import it.pagopa.interop.attributeregistrymanagement.model.persistence.serializer.v1.attribute.AttributeV1
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
+import it.pagopa.interop.attributeregistrymanagement.model.persistence.State
+import it.pagopa.interop.attributeregistrymanagement.model.persistence.serializer.v1.state.StateV1
+import it.pagopa.interop.attributeregistrymanagement.model.persistence.serializer.v1.state.StateEntryV1
+import PersistentSerializationSpec._
+import it.pagopa.interop.attributeregistrymanagement.model.persistence.serializer.{
+  PersistEventSerializer,
+  PersistEventDeserializer
+}
+import munit.Compare
 
 class PersistentSerializationSpec extends ScalaCheckSuite {
 
-//   property("State is correctly serialized") {
-//     forAll(stateGenerator) { case (state, stateV1) =>
-//       PersistEventSerializer.to[State, StateV1](state) === Right(stateV1)
-//     }
-//   }
+  property("State is correctly deserialized") {
+    forAll(stateGen) { case (state, stateV1) =>
+      assertEquals(PersistEventDeserializer.from[StateV1, State](stateV1), Either.right[Throwable, State](state))
+    }
+  }
+
+  property("State is correctly serialized") {
+    forAll(stateGen) { case (state, stateV1) =>
+      assertEquals(PersistEventSerializer.to[State, StateV1](state), Either.right[Throwable, StateV1](stateV1))
+    }
+  }
 }
 
 object PersistentSerializationSpec {
@@ -62,5 +77,35 @@ object PersistentSerializationSpec {
       creationTime = timeV1
     )
   )
+
+  val stateFromAttrs: List[PersistentAttribute] => State =
+    attrs => State(attrs.foldLeft(Map.empty[String, PersistentAttribute]) { case (m, a) => m + (a.id.toString -> a) })
+
+  val stateV1FromAttrs: List[AttributeV1] => StateV1 = attrsV1 => StateV1(attrsV1.map(a => StateEntryV1(a.id, a)))
+
+  val stateGen: Gen[(State, StateV1)] = Gen
+    .listOf(persistentAttributeGen)
+    .map(_.separate)
+    .map { case (attrs, attrsV1) => (stateFromAttrs(attrs), stateV1FromAttrs(attrsV1)) }
+
+  implicit val compareState: Compare[State, State] = (stateA, stateB) => {
+    stateA == stateB
+  }
+
+  implicit val compareStateV1: Compare[StateV1, StateV1] = (stateA, stateB) => {
+    stateA.attributes.sortBy(_.key) == stateB.attributes.sortBy(_.key)
+  }
+
+  implicit val compareStateEither: Compare[Either[Throwable, State], Either[Throwable, State]] = {
+    case (Right(stateA), Right(stateB)) => compareState.isEqual(stateA, stateB)
+    case (Left(a), Left(b))             => a == b
+    case _                              => false
+  }
+
+  implicit val compareStateV1Either: Compare[Either[Throwable, StateV1], Either[Throwable, StateV1]] = {
+    case (Right(stateA), Right(stateB)) => compareStateV1.isEqual(stateA, stateB)
+    case (Left(a), Left(b))             => a == b
+    case _                              => false
+  }
 
 }
