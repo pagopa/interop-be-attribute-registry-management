7c14681a270fdc76090d133e95f95f35ee9b11ca
Stefano Perazzolo <stefano.perazzolo@gmail.com>
2022-07-21 18:33:46 +0200

    update PagopaVersions

--- project/Versions.scala
+++ project/Versions.scala
@@ -19,6 +19,6 @@ object Versions {
 }
 
 object PagopaVersions {
-  lazy val commonsVersion    = "1.0.x-SNAPSHOT"
-  lazy val partyProxyVersion = "1.0.x-SNAPSHOT"
+  lazy val commonsVersion    = "1.0.3"
+  lazy val partyProxyVersion = "1.0.1"
 }
