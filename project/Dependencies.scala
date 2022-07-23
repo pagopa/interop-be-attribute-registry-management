cfe11c273aa7e4193cafa0b3bdc38ba38f3ddb94
Scala Steward <scala-steward@pagopa.it>
2022-07-23 06:48:02 +0000

    Update diffx-munit to 0.7.1 in 1.0.x

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -139,7 +139,7 @@ object Dependencies {
       akka.httpTestkit            % Test,
       "org.scalameta"            %% "munit"                % "1.0.0-M6" % Test,
       "org.scalameta"            %% "munit-scalacheck"     % "1.0.0-M6" % Test,
-      "com.softwaremill.diffx"   %% "diffx-munit"          % "0.7.0"    % Test
+      "com.softwaremill.diffx"   %% "diffx-munit"          % "0.7.1"    % Test
     )
     lazy val client: Seq[ModuleID]    =
       Seq(akka.stream, akka.http, akka.httpJson4s, akka.slf4j, json4s.jackson, json4s.ext, pagopa.commons).map(
