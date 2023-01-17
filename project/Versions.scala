893e9f85cd5b43cd3591a3d0536dd0fd1b9b8249
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2023-01-17 15:26:48 +0100

    bump versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -20,6 +20,6 @@ object Versions {
 }
 
 object PagopaVersions {
-  lazy val commonsVersion    = "1.0.x-SNAPSHOT"
-  lazy val partyProxyVersion = "1.0.x-SNAPSHOT"
+  lazy val commonsVersion    = "1.0.12"
+  lazy val partyProxyVersion = "1.0.4"
 }
