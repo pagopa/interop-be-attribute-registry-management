98fe4bb28aba1ffe38b141e3b2a836937c1a24ba
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2023-07-03 16:24:09 +0200

    fix pagopa versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -20,6 +20,6 @@ object Versions {
 }
 
 object PagopaVersions {
-  lazy val commonsVersion    = "1.0.17"
-  lazy val partyProxyVersion = "1.0.10"
+  lazy val commonsVersion    = "1.0.x-SNAPSHOT"
+  lazy val partyProxyVersion = "1.0.x-SNAPSHOT"
 }
